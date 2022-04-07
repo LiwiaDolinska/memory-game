927738d3f9ed3cf5e10b44212b4967b122ec5614
Liwia Dolinska <liwiamur@gmail.com>
2022-04-07 13:27:21 +0200

    changing padding and margin

--- src/components/StartGame/index.js
+++ src/components/StartGame/index.js
@@ -4,18 +4,22 @@ const StartPage = styled.div`
 color: white;
 background-color: black;
 text-align: center;
-font-size: 30px;
+font-size: 40px;
+height: 1000px;
+padding: 50px;
 `
-
 const StartGameWindow = styled.div`
 background-color: white;
-width: auto;
+width: 50%;
 height: 500px;
+padding: 10px;
+margin: auto;
+margin-top: 80px;
+
 `
 const ThemeToChoose = styled.h1`
 color: grey;
 font-size: 20px;
-
 `
 const OptionToChoose = styled.button`
 color: white;
@@ -27,28 +31,38 @@ margin: 4px 2px;
 display: inline-block;
 border-radius: 30px;
 `
+const StartButton = styled.button`
+color: white;
+background-color: orange;
+padding: 15px 32px;
+font-size: 30px;
+border-radius: 30px;
+border: orange;
+margin: 40px;
+`
 
 export function StartGame() {
     return <>
         <StartPage>Start game Memory!
             <StartGameWindow>
                 <ThemeToChoose>Select Theme</ThemeToChoose>
-                <>
+                <div>
                     <OptionToChoose>Numbers</OptionToChoose>
                     <OptionToChoose>Icons</OptionToChoose>
-                </>
+                </div>
                 <ThemeToChoose>Number of players</ThemeToChoose>
-                <>
+                <div>
                     <OptionToChoose>1 player</OptionToChoose>
                     <OptionToChoose>2 players</OptionToChoose>
                     <OptionToChoose>3 players</OptionToChoose>
                     <OptionToChoose>4 players</OptionToChoose>
-                </>
+                </div>
                 <ThemeToChoose>Size of game</ThemeToChoose>
-                <>
+                <div>
                     <OptionToChoose>4*4</OptionToChoose>
                     <OptionToChoose>6*6</OptionToChoose>
-                </>
+                </div>
+                <StartButton>Start game!</StartButton>
             </StartGameWindow>
         </StartPage>
     </>
